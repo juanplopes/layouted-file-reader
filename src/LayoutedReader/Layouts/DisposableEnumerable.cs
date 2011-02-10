26040b706189a788bed00d67aa5f76e46ae67708
Juan Lopes <me@juanlopes.net>
2011-02-10 11:09:08 -0200

    disposableenumerable

---
+++ src/LayoutedReader/Layouts/DisposableEnumerable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LayoutedReader.Layouts
+{
+    public class DisposableEnumerable<T> : IDisposable, IEnumerable<T>
+    {
+        IEnumerable<T> enumerable;
+        public DisposableEnumerable(IEnumerable<T> enumerable)
+        {
+            this.enumerable = enumerable;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return enumerable.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return enumerable.GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+            if (enumerable is IDisposable)
+                (enumerable as IDisposable).Dispose();
+        }
+    }
+}
