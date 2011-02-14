2c10e8df4e499d543d5e870acf6e875b135f6840
Juan Lopes <me@juanlopes.net>
2011-02-14 19:24:51 -0200

    new build commit

--- build.cmd
+++ build.cmd
@@ -3,6 +3,9 @@ setlocal
 set BUILD_TARGET=%~1
 if "%BUILD_TARGET%"=="" set /p BUILD_TARGET="Build Target: "
 if not "%BUILD_TARGET%"=="" set BUILD_TARGET="/target:%BUILD_TARGET%"
-util\msbuild35 build.xml %BUILD_TARGET% %2 %3 %4 %5 %6 %7 %8 %9
+%~dp0\util\msbuild35 %~dp0\build.xml %BUILD_TARGET% %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
-if errorlevel 1 pause
+if errorlevel 1 ( 
+ echo FAIL >&2
+ if "%~1"=="" pause
+)
